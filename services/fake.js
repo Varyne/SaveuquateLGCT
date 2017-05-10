8153d8bd2fb232c6dc97cce3ae4d8b2f01882e7a
EveX1 <evekill@free.fr>
2017-05-10 16:06:39 +0200

    qualities' array faker fix

--- services/fake.js
+++ services/fake.js
@@ -11,22 +11,25 @@ const db = mongoose.connection;
 
 db.on('open', () => {
     console.log("Connected to MongoDB/kittens");
-    KittenModel.remove({});
+    KittenModel.remove({}, function(err) {
+        console.log('collection removed')
+    });
     seed(50);
 });
 
-// let items = Math.floor((Math.random() * 3) + 1);
-// let qualities = []
-// for (var j = 0; j < items; j++) {
-//     qualities.push(faker.company.catchPhraseAdjective())
-// }
 
 const seed = (count) => {
     for (var i = 0; i < count; i++) {
+        let items = Math.floor((Math.random() * 3) + 1);
+        let qualities = []
+        for (var j = 0; j < items; j++) {
+            qualities.push(faker.company.catchPhraseAdjective())
+        }
+        console.log(qualities)
         KittenModel.create({
             "name": faker.name.lastName(),
             "color": faker.commerce.color(),
-            "qualities": faker.company.catchPhraseAdjective(),
+            "qualities": qualities,
             "flaw": faker.hacker.adjective(),
             "foodBrand": faker.company.companyName(),
         });
