fff19e58f29f0bde7531e20dcc8cf98c26445a80
EveX1 <evekill@free.fr>
2017-05-10 15:16:38 +0200

    random seed

--- services/fake.js
+++ services/fake.js
@@ -1,32 +1,34 @@
 const mongoose = require('mongoose');
 const faker = require('faker/locale/fr');
 
-const KittenModel = require('../models/kittens')
+const KittenModel = require('../models/ModelKittens');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/kittens')
+mongoose.connect('mongodb://localhost/kittens');
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
 db.on('open', () => {
-    console.log("Connected to MongoDB/kittens")
+    console.log("Connected to MongoDB/kittens");
+    KittenModel.remove({});
+    seed(50);
+});
 
-    KittenModel.create([{
-            "name": "Alice",
-            "color": "Aubergine",
-            "qualities": ["Agile"],
-            "flaw": "Agressive",
-            "foodBrand": "AlloCat",
-        },
-        {
-            "name": "Bob",
-            "color": "Brun",
-            "qualities": ["Beau", "Bon caractère"],
-            "flaw": "Borné",
-            "foodBrand": "BibiCroket",
-            "adopted": true
-        }
-    ])
+// let items = Math.floor((Math.random() * 3) + 1);
+// let qualities = []
+// for (var j = 0; j < items; j++) {
+//     qualities.push(faker.company.catchPhraseAdjective())
+// }
 
-})
+const seed = (count) => {
+    for (var i = 0; i < count; i++) {
+        KittenModel.create({
+            "name": faker.name.lastName(),
+            "color": faker.commerce.color(),
+            "qualities": faker.company.catchPhraseAdjective(),
+            "flaw": faker.hacker.adjective(),
+            "foodBrand": faker.company.companyName(),
+        });
+    }
+}
--- services/kittens.js
+++ services/kittens.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+ObjectId = mongoose.Types.ObjectId
 
 const KittenModel = require('../models/ModelKittens')
 
@@ -9,16 +10,61 @@ mongoose.connect('mongodb://localhost/kittens')
 const db = mongoose.connection
 
 db.on('open', () => {
-    console.log("Connected to MongoDB/kittens")
+    console.error("Connected to MongoDB/kittens");
+})
 
+function getAll() {
     KittenModel.find({})
         .then(
-            results => console.log(results)
-        ).catch(e => console.log(e))
+            results => { return results }
+        ).catch(e => console.error(e))
+}
 
-    KittenModel.find({ color: "Brun" })
+function getColors() {
+    KittenModel.find({}, { color: 1, _id: 0 })
         .then(
-            results => console.log(results)
-        ).catch(e => console.log(e))
+            results => { return results }
+        ).catch(e => console.error(e))
+}
 
-})
+function getByAdoptStatus(boolean) {
+    KittenModel.find({ adopted: boolean }, { name: 1, _id: 0 })
+        .then(
+            results => { return results }
+        ).catch(e => console.error(e))
+}
+
+function getById(id) {
+    KittenModel.findById(id)
+        .then(
+            result => { return result }
+        ).catch(e => console.error(e))
+}
+
+function getByName(ReqName) {
+    KittenModel.find({ name: ReqName })
+        .then(
+            result => { return result }
+        ).catch(e => console.error(e))
+}
+
+function getByColor(reqColor) {
+    KittenModel.find({ color: reqColor })
+        .then(
+            results => { return results }
+        ).catch(e => console.error(e))
+}
+
+function adopt(id) {
+    KittenModel.update({ _id: id }, { $set: { adopted: true } })
+        .then(
+            results => { return results }
+        ).catch(e => console.error(e))
+}
+
+function addTalent(id, quality) {
+    KittenModel.findByIdAndUpdate(id, { $push: { qualities: quality } })
+        .then(
+            results => { return results }
+        ).catch(e => console.error(e))
+}
